1a54bdf190c052b80134f30fc506895645f15d2c
Sumithra Bai S <sumisoni2002@gmail.com>
2023-07-17 11:01:58 +0530

    added

--- src/main/java/todo/dto/Task.java
+++ src/main/java/todo/dto/Task.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 import lombok.Data;
 
 @Entity
-@Data
+
 @Component
 public class Task {
 
@@ -23,5 +23,35 @@ public class Task {
 	String description;
 	LocalDate date;
 	boolean status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
 
 }
--- src/main/java/todo/dto/UserInfo.java
+++ src/main/java/todo/dto/UserInfo.java
@@ -12,7 +12,7 @@ import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
-@Data
+
 public class UserInfo {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,4 +24,45 @@ public class UserInfo {
 
 	@OneToMany
 	List<Task> tasks;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
 }
